bdc2bd5fb9fe3133ce2bb5d5d8b6684e61cb760b
Yunus Emre <yunuspider@gmail.com>
2025-07-08 19:43:34 +0300

    Completed the exercise 2.20

--- part2/restcountries/src/components/CountryCard.jsx
+++ part2/restcountries/src/components/CountryCard.jsx
@@ -1,4 +1,15 @@
+import { useEffect, useState } from 'react'
+import weatherProvider from '../services/weatherProvider'
+
 const CountryCard = ({ country }) => {
+  const [weather, setWeather] = useState({})
+
+  useEffect(() => {
+    weatherProvider.get(country.capital[0]).then(data => {
+      setWeather(data)
+    })
+  }, [])
+
   return (
     <>
       <h1>{country.name.common} ({country.name.official}) {country.flag}</h1>
@@ -9,8 +20,16 @@ const CountryCard = ({ country }) => {
       <ul>
         {Object.entries(country.languages).map(([key, value]) => <li key={key}>{value}</li>)}
       </ul>
-      
+
       <img src={country.flags.svg} alt={country.flags.alt} width="400" />
+
+      <h2>Weather in {country.capital[0]}</h2>
+      <p>{weather.description}</p>
+      <div>Temperature: {weather.temp}</div>
+      <div>
+        <img src={weather.iconUrl} alt='The weather' />
+      </div>
+      <div>Wind: {weather.wind}</div>
     </>
   )
 }
---
+++ part2/restcountries/src/services/weatherProvider.js
@@ -0,0 +1,30 @@
+import axios from "axios"
+
+const api_key = import.meta.env.VITE_WEATHER_API
+const geoCodingBaseUrl = "http://api.openweathermap.org/geo/1.0"
+const weatherBaseUrl = "https://api.openweathermap.org/data/2.5"
+
+const get = (city) => {
+  return axios.get(`${geoCodingBaseUrl}/direct?q=${city}&limit=1&appid=${api_key}`)
+    .then(response => {
+      const lat = response.data[0].lat
+      const lon = response.data[0].lon
+
+      return axios.get(`${weatherBaseUrl}/weather?lat=${lat}&lon=${lon}&appid=${api_key}`)
+    })
+    .then(response => {
+      const weatherData = {
+        temp: `${(response.data.main.temp - 273.15).toFixed(1)} Celsius`,
+        wind: `${response.data.wind.speed} m/s`,
+        description: response.data.weather[0].description,
+        iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`
+      }
+
+      return weatherData
+    })
+    .catch(error => {
+      console.error(error)
+    })
+}
+
+export default { get }
