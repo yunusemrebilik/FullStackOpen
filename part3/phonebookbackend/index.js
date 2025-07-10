51478761a8125e2f4ca1d5f1676ae09b248b19be
Yunus Emre <yunuspider@gmail.com>
2025-07-10 17:54:53 +0300

    Completed the exercise 3.3

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -28,6 +28,17 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 app.get('/info', (request, response) => {
   response.send(`
     <div>
