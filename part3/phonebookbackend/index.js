4f97bdec3214047e52a3843ef675b61b332a5624
Yunus Emre <yunuspider@gmail.com>
2025-07-10 18:23:15 +0300

    Completed the exercise 3.6

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -71,9 +71,16 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
+
   if (!body.name || !body.number) {
     return response.status(400).json({
-      "error": "name or number missing"
+      error: "name or number missing"
+    })
+  }
+
+  if (persons.find(p => p.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique"
     })
   }
  
