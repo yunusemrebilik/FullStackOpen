64cbd6751725bc2f92ce5c56f1177b0fadea39ba
Yunus Emre <yunuspider@gmail.com>
2025-07-28 17:46:19 +0300

    Completed the exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0)
+    return 0
+  return blogs.reduce((acc, cur) => acc + cur.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
