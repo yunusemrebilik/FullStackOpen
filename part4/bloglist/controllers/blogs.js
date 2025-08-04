6f1d5bf5add88a1789c4913d958224f1b7717d55
Yunus Emre <yunuspider@gmail.com>
2025-08-04 18:29:50 +0300

    Completed the exercise 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,14 +1,18 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogRouter.post('/', async (request, response) => {
   try {
     const blog = new Blog(request.body)
+    const user = await User.findOne({})
+
+    blog.user = blog.user ?? user._id
     const savedBlog = await blog.save()
     response.status(201).json(savedBlog)
   } catch (exception) {
