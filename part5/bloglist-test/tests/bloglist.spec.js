04c7740f5abfaf8b20452f0f482e863169869c97
Yunus Emre <yunuspider@gmail.com>
2025-08-15 15:53:29 +0300

    Completed the exercise 5.17

---
+++ part5/bloglist-test/tests/bloglist.spec.js
@@ -0,0 +1,14 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('log in to application')).toBeVisible()
+    await expect(page.getByRole('textbox', { name: 'username' })).toBeVisible()
+    await expect(page.getByRole('textbox', { name: 'password' })).toBeVisible()
+    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+})
